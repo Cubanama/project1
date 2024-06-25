c9d6018d0cb2e0a52f8542b098ede72f001f5d31
Cubanama <156826917+Cubanama@users.noreply.github.com>
2024-06-24 22:11:30 -0400

    Done!

---
+++ src/main/java/edu/mdc/cop2805c/assignment1/interfaces/FossilFuelVehicle.java
@@ -0,0 +1,15 @@
+package edu.mdc.cop2805c.assignment1.interfaces;
+
+/**
+ *
+ * @author cubaa
+ */
+public interface FossilFuelVehicle {
+    
+public abstract double estimateFuelEfficiency();
+
+public abstract double estimateCO2Emissions();
+
+public abstract double estimateNOxEmissions(); 
+}
+
